ef38e03987307466cb563b2eb55995e76431b2e9
msemionova <maria.semionova@yahoo.com>
2021-04-25 19:47:23 +0300

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders TODO heading', () => {
-  render(<App />);
-  const headingElement = screen.getByText(/TODOs/i);
-  expect(headingElement).toBeInTheDocument();
-});
